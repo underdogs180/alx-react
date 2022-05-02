71c23c277c43db3711ada73d07efea50ddcb625b
underdogs180 <asadirshad38@gmailcom>
2022-05-02 16:23:23 +0000

    Task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require(ath');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.jpg$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  }
+};
