a666003a5359f122729aac9063f79107cf209f4e
underdogs180 <asadirshad38@gmailcom>
2022-05-04 15:14:57 +0000

    Updates

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+	    <div className='App'>
+	    <header className='App-header'>
+	    <img src={logo} className='App-logo' alt='logo' />
+	    <h1>School dashboard</h1>
+	    </header>
+	    <body className='App-body'>
+	    <p>Login to access the full dashboard</p>
+	    <label htmlFor='email'>Email:</label>
+	    <input type='email' name='email' id='email' />
+
+	    <label htmlFor='password'>Password:</label>
+	    <input type='password' name='password' id='password' />
+
+	    <button>OK</button>
+	    </body>
+	    <footer className='App-footer'>
+	    <p>{`Copyright ${getFullYear()} - ${getFooterCopy(true)}`}</p>
+	    </footer>
+	    </div>
+    );
 }
 
 export default App;
--- 0x01-react_intro/task_2/dashboard/src/index.js
+++ 0x01-react_intro/task_2/dashboard/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Notifications from './Notifications';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+	<React.StrictMode>
+	<App />
+	<Notifications />
+	</React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
