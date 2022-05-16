09350477d73ad43bd829d6d4b510928dc1283b66
underdogs180 <asadirshad38@gmailcom>
2022-05-16 17:03:32 +0000

    Task 1 Completed

--- 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/App/App.js
@@ -1,73 +1,88 @@
-import React from 'react';
-import './App.css';
+mport React, { Component, Fragment } from 'react';
 import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Notification from '../Notifications/Notifications';
 import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
 import CourseList from '../CourseList/CourseList';
-import { getLatestNotification } from '../utils/utils';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
 import PropTypes from 'prop-types';
+import { getLatestNotification } from '../utils/utils';
+import { StyleSheet, css } from 'aphrodite';
 
-
-class App extends React.Component {
-  static listCourses = [
-    {id: 1, name: 'ES6', credit: 60},
-    {id: 2, name: 'Webpack', credit: 20},
-    {id: 3, name: 'React', credit: 40}
-  ];
-
-  static listNotifications = ]
-    {id: 1, value: "New course available", type: "default"},
-    {id: 2, value: "New resume available", type: "urgent"},
-    {id: 3, html: {__html: getLatestNotification()}, type: "urgent"},
-  ];
-
+class App extends Component {
   constructor(props) {
     super(props);
-    this.isLoggedIn = props.isLoggedIn;
-    this.logOut = props.logOut;
-    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
-
-  handleKeyDown(e) {
-    e.preventDefault();
-    if (e.ctrlKey && e.key === 'h') {
-	alert("Logging you out");
-	this.logOut();
-    }  
-  }
-
   componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('keydown', this.handleLogout);
   }
-
   componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
+    window.removeEventListener('keydown', this.handleLogout);
   }
-
-  render () {
+  handleLogout(e) {
+    if (e.ctrlKey && e.key === 'h') {
+      e.preventDefault();
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+  render() {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    ];
+    const { isLoggedIn } = this.props;
     return (
-      <React.Fragment>
-	<Notification listNotifications={this.listNotifications}/>
-	<div className="App">
-	  <Header />
-	  {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses}/> : <Login />}
-	  <Footer />
-	</div>
-      </React.Fragment>
-    );
-  }
+      <Fragment>
+	 <Notifications listNotifications={listNotifications} />
+         <Header />
+         {isLoggedIn ? (
+	   <BodySectionWithMarginBottom title='Course list'>
+	     <CourseList listCourses={listCourses} />
+           </BodySectionWithMarginBottom>
+         ) : (
+	   <BodySectionWithMarginBottom title='Log in to continue'>
+	     <Login />
+	   </BodySectionWithMarginBottom>
+         )}
+         <BodySection title='News from the School'>
+	   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	 </BodySection>
+         <div className={css(styles.footer)}>
+           <Footer />
+          </div>
+	</Fragment>
+      );
+   }
 }
 
 App.defaultProps = {
   isLoggedIn: false,
-  logOut: () => {}
+  logOut: () => undefined,
 };
 
 App.propTypes = {
   isLoggedIn: PropTypes.bool,
-  logOut: PropTypes.func
+  logOut: PropTypes.func,
 };
 
-export default App;
+const styles = StyleSheet.create({
+  footer: {
+    width: '100%',
+    position: 'fixed',
+    bottom: 0,
+    textAlign: 'center',
+    fontStyle: 'italic',
+    borderTop: 'thick solid #e0344a',
+  },
+});
 
+export default App;
--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,14 +1,31 @@
 import React from 'react';
-import logo from '../assets/holberton_logo.jpg';
-import './Header.css';
+import logo from '../assets/holberton-logo.jpg';
+import { StyleSheet, css } from 'aphrodite';
 
 function Header() {
     return (
-	    <div className="App-header">
-	    <img src={logo} alt="logo" />
-	    <h1>School dashboard</h1>
-	    </div>
-    )
+	    <header className={css(styles.header)}>
+	    <img className={css(styles.logo)} src={logo} alt='logo' />
+	    <h1 className={css(styles.title)}>School dashboard</h1>
+	    </header>
+    );
 }
+
+const styles = StyleSheet.create({
+    header: {
+	display: 'flex',
+	color: '#e0344a',
+	alignItems: 'center',
+	borderBottom: 'thick solid #e0344a',
+	width: '100%',
+	position: 'fixed',
+    },
+    logo: {
+	width: '144px',
+    },
+    title: {
+	margin: 0,
+    },
+});
 
 export default Header;
