33ecd113af846701696e6d1bac208fbc107a9019
underdogs180 <asadirshad38@gmailcom>
2022-05-22 09:07:31 +0000

    Task 2 Completed

--- 0x05-react_state/task_2/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_2/dashboard/src/Header/Header.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import logo from '../assets/holberton_logo.jpg';
-import './Header.css';
+import { StyleSheet, css } from 'aphrodite';
+import logo from '../assets/holberton-logo.jpg';
+import AppContext from '../App/AppContext';
 
-function Header() {
-    return (
-	    <div className="App-header">
-	    <img src={logo} alt="logo" />
-	    <h1>School dashboard</h1>
-	    </div>
-    )
+class Header extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		const { user, logOut } = this.context;
+		return (
+			<header className={css(styles.Header)}>
+				<img src={logo} className={css(styles.HeaderLogo)} alt="logo" />
+				<h1>School dashboard</h1>
+				{
+					user.isLoggedIn && (
+						<h2 id='logoutSection'>
+							Welcome <b>{user.email}</b>
+							<span onClick={logOut} className={css(styles.logOutSpanSection)}>
+								(logout)
+							</span>
+						</h2>
+					)
+				}
+			</header>
+		);
+	}
 }
+
+Header.contextType = AppContext;
+
+const styles = StyleSheet.create({
+	Header: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		color: '#e1354b',
+		borderBottom: '4px solid #e1354b'
+	},
+
+	HeaderLogo: {
+		width: '20%'
+	},
+
+	logOutSpanSection: {
+		cursor: 'pointer',
+		fontStyle: 'italic'
+	},
+});
 
 export default Header;
