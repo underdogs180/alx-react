42ce2768d86969819e76b9232c633fc8d3891b3e
underdogs180 <asadirshad38@gmailcom>
2022-06-13 17:28:46 +0000

    Task 14 to 16

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/App/App.test.js
@@ -1,51 +1,78 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from './App';
+import { StyleSheetTestUtils } from 'aphrodite';
+import { mapStateToProps } from './App';
+import { fromJS } from 'immutable';
+import configureStore from 'redux-mock-store'
+import { initialState } from '../reducers/rootReducer';
+import { Provider } from 'react-redux';
+
+const mockStore = configureStore([]);
 
 describe('Test App.js', () => {
-    let wrapper;
+  let wrapper;
+  let store;
 
-    beforeEach(() => {
-	wrapper = shallow(<App />);
-    });
+  beforeEach(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+    store = mockStore(initialState);
+    wrapper = mount(<Provider store={store} ><App/></Provider>);
+  });
 
-    it('Renders App without crashing', () => {
-	expect(wrapper.exists());
-    });
+  it('Renders App without crashing', () => {
+    expect(wrapper.exists());
+  });
 
-    it('App component contains Notifications component', () => {
-	expect(wrapper.find("Notifications")).toHaveLength(1);
-    });
+  it('App component contains Notifications component', () => {
+    expect(wrapper.find("Notifications")).toHaveLength(1);
+  });
 
-    it('App component contains Header component', () => {
-	expect(wrapper.find("Header")).toHaveLength(1);
-    });
+  it('App component contains Header component', () => {
+    expect(wrapper.find("Header")).toHaveLength(1);
+  });
 
-    it('App component contains Login component', () => {
-	expect(wrapper.find("Login")).toHaveLength(1);
-    });
+  it('App component contains Footer component', () => {
+    expect(wrapper.find("Footer")).toHaveLength(1);
+  });
 
-    it('App component contains Footer component', () => {
-	expect(wrapper.find("Footer")).toHaveLength(1);
-    });
-
-    it('test to check that CourseList is not displayed inside App', () => {
-	expect(wrapper.find("CourseList")).toHaveLength(0);
-    });
+  it('test to check that CourseList is not displayed inside App', () => {
+    expect(wrapper.find("CourseList")).toHaveLength(0);
+  });
 });
 
-describe("Testing <App isLoggedIn={true} />", () => {
-    let wrapper;
+describe('Testing mapStateToProps', () => {
+  it('test that verify that the function returns the right object', () => {
+    let state = {
+      ui: fromJS({
+        isUserLoggedIn: true,
+      }),
+    };
+    expect(mapStateToProps(state)).toEqual(expect.objectContaining({ isLoggedIn: true }));
+  });
+});
 
-    beforeEach(() => {
-	wrapper = shallow(<App isLoggedIn={true}/>);
-    });
+describe("Testing rootReducer", () => {
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = {
+      ui: fromJS({
+        isUserLoggedIn: true,
+      }),
+    };
 
-    it("the Login component is not included", () => {
-	expect(wrapper.find('Login')).toHaveLength(0);
-    });
+    const result = mapStateToProps(state);
 
-    it(" the CourseList component is included", () => {
-	expect(wrapper.find('CourseList').exists());
-    });
+    expect(result).toEqual({ isLoggedIn: true });
+  });
+  it("mapStateToProps returns the right object from display Drawer", () => {
+    let state = {
+      ui: fromJS({
+        isNotificationDrawerVisible: true,
+      }),
+    };
+
+    const result = mapStateToProps(state);
+
+    expect(result).toEqual({ displayDrawer: true });
+  });
 });
