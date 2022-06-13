dfebdce162a3ffe95c69e6231369169e7e19a527
underdogs180 <asadirshad38@gmailcom>
2022-06-13 18:17:03 +0000

    Tasks

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/actions/notificationActionCreators.js
@@ -1,4 +1,11 @@
-import { MARK_AS_READ, SET_TYPE_FILTER } from './notificationActionTypes';
+import {
+  MARK_AS_READ,
+  SET_TYPE_FILTER,
+  SET_LOADING_STATE,
+  FETCH_NOTIFICATIONS_SUCCESS,
+} from "./notificationActionTypes";
+
+import "node-fetch";
 
 export const markAsAread = (index) => {
   return {
@@ -7,9 +14,39 @@ export const markAsAread = (index) => {
   };
 };
 
+export const boundMarkAsAread = (index) => dispatch(markAsAread(index));
+
 export const setNotificationFilter = (filter) => {
   return {
     type: SET_TYPE_FILTER,
     filter,
+  };
+};
+
+export const boundSetNotificationFilter = (filter) =>
+  dispatch(setNotificationFilter(filter));
+
+export const setLoadingState = (loading) => {
+  return {
+    type: SET_LOADING_STATE,
+    loading,
+  };
+};
+
+export const setNotifications = (data) => {
+  return {
+    type: FETCH_NOTIFICATIONS_SUCCESS,
+    data,
+  };
+};
+
+export const fetchNotifications = () => {
+  return (dispatch) => {
+    dispatch(setLoadingState(true));
+    return fetch("./notifications.json")
+      .then((res) => res.json())
+      .then((data) => dispatch(setNotifications(data)))
+      .catch((error) => {})
+      .finally(() => dispatch(setLoadingState(false)));
   };
 };
